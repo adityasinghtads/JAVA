be8468e057083a9576399bb468b1accba7694f9c
Aditya Singh <thakur.11913421@lpu.in>
2022-11-12 23:10:13 +0530

    Learning Hello world

---
+++ Basics/MyFirstProgram.java
@@ -0,0 +1,25 @@
+
+class MyFirstProgram {
+public static void main(String[] args) {
+    System.out.println("Hello World");
+  }
+}
+
+/*
+ * First in Java we have class in java 
+ * -- Like in every language we have a main class 
+ * -- Inside class we have main method - serves as the entry point 
+ * 
+ * For entry point it looks for a public class with static method with name main and have a string which dont return any value. 
+ * 
+ * So we write is 
+ * public static void main(string[]args){
+ * 
+ * }
+ * 
+ * ** Semi colons is mandatory in java ; ( No as python )
+ *  
+ */
+
+
+
