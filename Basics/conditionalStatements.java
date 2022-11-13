187a41b253be70b25e7d8bab9f0def43ee97d779
Aditya Singh <thakur.11913421@lpu.in>
2022-11-13 23:46:06 +0530

    If-else conditional statements

--- Basics/conditionalStatements.java
+++ Basics/conditionalStatements.java
@@ -9,14 +9,14 @@ public class conditionalStatements {
     System.out.println("Enter the second number :");
     double num2 = scanner.nextDouble();
     scanner.nextLine();
-    
+
     System.out.printf("What operation due to want to perform:");
     String operation = scanner.nextLine();
 
-    if (operation.equals("sum")){
-      System.out.printf("%d + %d = %d",num1,num2,num1+num2);
-    } else{
-      System.out.println("This operation is not supported");
+    if (operation.equals("sum"))  {
+      System.out.printf("%d + %d = %d",  num1 , num 2, n u m1 + num2);
+    } else  {
+      System.out.println("This operation is not supported")
     }
     scanner.close();
   }
