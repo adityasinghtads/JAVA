7d17d4c8c4f3276ebb31b244eb512fc2c8bb528e
AdityaSingh295 <>
2022-11-14 07:52:56 +0000

    Java practice

---
+++ practice1.java
@@ -0,0 +1,26 @@
+public class practice{
+  public static void main(String[] args){
+    System.out.println("Day-1 Java practice");
+
+    // Hello World 
+    System.out.println("The famous Hello World Program in Java");
+    
+    // variables in Java 
+    int age = 21; 
+    int number = 123;
+    double Dnumber = 1.345;
+    
+    
+    // Datatypes in Java 
+
+    // operators in Java 
+
+    // Strings in Java 
+
+    // user inputs in java 
+
+    // Conditional statements 
+    
+  }
+}
+
