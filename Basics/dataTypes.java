89fb6e0845c86cfc67d299b574fd00b7a6a1dbf8
Aditya Singh <thakur.11913421@lpu.in>
2022-11-27 23:17:08 +0530

    Updated - BigInteger type

--- Basics/dataTypes.java
+++ Basics/dataTypes.java
@@ -1,3 +1,5 @@
+import java.math.BigInteger;
+
 /*
  * Data types in Java : 
  * ** Primitive and Non Primitive(refrence). 
@@ -9,25 +11,25 @@
  */
 
 class dataTypes {
-  public static void main(String[] args){
+  public static void main(String[] args) {
     /*
-     * Primitive types are 8 types 
+     * Primitive types are 8 types
      */
-    // Integer types 
-    byte aSingleByte =100;
-    short aSmallNumber = 20000; 
+    // Integer types
+    byte aSingleByte = 100;
+    short aSmallNumber = 20000;
     int anInteger = 2113344556;
     long aLargeNumber = 9222222222222222L;
 
-    // Decimal types 
-    double aDouble = 1.797989898; 
+    // Decimal types
+    double aDouble = 1.797989898;
     float aFloat = 3.0498F;
 
-    //boolens
+    // boolens
     boolean isWeekend = false;
     boolean isWorkday = true;
-    
-    //characters 
+
+    // characters
     char copyrightsymbol = '\u00A9';
 
     System.out.println(isWorkday);
@@ -40,32 +42,35 @@ class dataTypes {
     System.out.println(aSingleByte);
 
     System.out.println("This is the copyright symbol " + copyrightsymbol);
-  
-    
-/* 
-* Type conversion can be implicit or explicit : 
--- Expiclit conversion can also lead to loss of data 
-* 
-*/
 
-// Int ->double 
-    int num1 = 5;  
-    double num2=num1;
+    // Used in case when we have massive numbers.
+    BigInteger hum = new BigInteger("9999999999999999999999999999999");
+    BigInteger hum1 = new BigInteger("8888888888888888888888888888888");
+    BigInteger sum = hum.subtract(hum1);
+    System.out.println(sum);
+
+    /*
+     * Type conversion can be implicit or explicit :
+     * -- Expiclit conversion can also lead to loss of data
+     * 
+     */
+
+    // Int ->double
+    int num1 = 5;
+    double num2 = num1;
     System.out.println(num2);
     System.out.println(num1);
 
-    // Double -> int , cant be done directly we need to add the int type before converting 
+    // Double -> int , cant be done directly we need to add the int type before
+    // converting
 
-    double n1=9.8;
+    double n1 = 9.8;
     System.out.println(n1);
-    // Output = 9.8 
-    int n2 = (int)n1 ; // This will work and will change it to int accordingly 
-    // Output = 9 
+    // Output = 9.8
+    int n2 = (int) n1; // This will work and will change it to int accordingly
+    // Output = 9
     System.out.println(n2);
 
   }
-    
+
 }
-
-
-
---
+++ Practice/practice4.java
@@ -0,0 +1,43 @@
+import java.math.BigInteger;
+
+public class practice4 {
+  /*
+   * Practicing Java- All basics covered till now.
+   */
+  public static void main(String[] args) {
+    System.out.println("My First program in Java");
+
+    dataTypes();
+  }
+
+  static void dataTypes() {
+    int num = 10;
+    System.out.println(num);
+    float num1 = 8.7f;
+    System.out.println((int) num1); // To convert to int
+
+    // Seal - Upr - Greater number - Float n double
+    // Floor - Neeche - lesser number - Float n double
+
+    System.out.println(Math.ceil(num1));
+    System.out.println(Math.floor(num1));
+
+    int maxPossible = Integer.MAX_VALUE;
+    System.out.println(maxPossible);
+
+    System.out.println(Math.pow(2, 4));
+    System.out.println(Math.pow(2, 8));
+    System.out.println(256/2);
+
+    // Used in case when we have massive numbers. 
+
+    BigInteger hum = new BigInteger("9999999999999999999999999999999");
+    BigInteger hum1 = new BigInteger("8888888888888888888888888888888");
+    BigInteger  sum = hum.subtract(hum1);
+    System.out.println(sum);
+
+
+  }
+
+
+}
