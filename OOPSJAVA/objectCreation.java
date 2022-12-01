0a69199cc78362249918631d369d624e739173f5
Aditya Singh <thakur.11913421@lpu.in>
2022-12-01 23:20:17 +0530

    Methods and anonymous methods

---
+++ OOPSJAVA/objectCreation.java
@@ -0,0 +1,15 @@
+public class objectCreation {
+  public static void main(String[] args) {
+
+  }
+}
+
+/*
+ * By new Keyword
+ * By newInstance() method
+ * By clone() method 
+ * By deserialization 
+ * By factory method etc. 
+ */
+
+ 
