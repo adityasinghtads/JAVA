1598e9540721a3d38efca7c15c402a72fa69832f
Aditya Singh <t.adityadhanrajsingh2001@gmail.com>
2022-12-07 17:10:27 +0530

    In main branch

---
+++ Practice/practice8.java
@@ -0,0 +1,20 @@
+public class practice8 {
+    
+}
+
+class practice{
+    practice(){
+        System.out.println("Hey hello");
+        int hey = 10;
+        System.out.println("hey");
+    }
+    public static void main(String[] args) {
+        practice hey = new practice();
+        // Constructor is called every time when a object is made 
+        /*
+         * if we dont define the constructor in that case java makes a default constructor and uses the same 
+         * 
+         */
+        
+    }
+}
