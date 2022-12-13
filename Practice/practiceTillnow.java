2a59a4dd0ed876ec59252c3f354ee7eeedffe647
Aditya Singh <thakur.11913421@lpu.in>
2022-12-13 23:31:24 +0530

    practice

--- Practice/practiceTillnow.java
+++ Practice/practiceTillnow.java
@@ -15,3 +15,35 @@ public class practiceTillnow {
     System.out.println("This is a nonstatic function ");
   }
 }
+
+
+class conditionalStatements{
+  public static void main(String[] args) {
+    HeyIfElse(2);
+  }
+
+  static void HeyIfElse(int i){
+    if (i%2==0){
+      System.out.println("The number given is even");
+    }else{
+      System.out.println("Its odd");
+    }
+  }
+
+}
+
+
+class loops{
+  public static void main(String[] args) {
+    itsLooping();  
+  }
+
+  static void itsLooping(){
+    int myArr[] = {1,2,3,4,5,6,7};
+    for (int i = 0; i < myArr.length; i++) {
+      System.out.println(i+1);
+    }
+  }
+
+}
+
