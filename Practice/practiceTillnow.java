07d36be9615917f7703f1855d33edc6398fd6d94
Aditya Singh <thakur.11913421@lpu.in>
2022-12-13 23:24:33 +0530

    Practice overall

---
+++ Practice/practiceTillnow.java
@@ -0,0 +1,17 @@
+public class practiceTillnow {
+  public static void main(String[] args) {
+    System.out.println("Hey hello lets start the practice.");
+    // making funtion
+    heyFunction();
+    practiceTillnow wip = new practiceTillnow();
+    wip.nonStatic();
+  }
+
+  static void heyFunction() {
+    System.out.println("Hey its into the function ");
+  }
+
+  void nonStatic() {
+    System.out.println("This is a nonstatic function ");
+  }
+}
