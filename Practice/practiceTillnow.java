d8e62d680ebb54e0d65a5fa6fbdbdd413f8cf47a
Aditya Singh <thakur.11913421@lpu.in>
2022-12-15 23:04:58 +0530

    Practice java

--- Practice/practiceTillnow.java
+++ Practice/practiceTillnow.java
@@ -1,3 +1,9 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import javax.naming.ldap.SortControl;
+import javax.swing.SpinnerDateModel;
+
 public class practiceTillnow {
   public static void main(String[] args) {
     System.out.println("Hey hello lets start the practice.");
@@ -16,34 +22,88 @@ public class practiceTillnow {
   }
 }
 
-
-class conditionalStatements{
+class conditionalStatements {
   public static void main(String[] args) {
     HeyIfElse(2);
   }
 
-  static void HeyIfElse(int i){
-    if (i%2==0){
+  static void HeyIfElse(int i) {
+    if (i % 2 == 0) {
       System.out.println("The number given is even");
-    }else{
+    } else {
       System.out.println("Its odd");
     }
   }
 
 }
 
-
-class loops{
+class loops {
   public static void main(String[] args) {
-    itsLooping();  
+    itsLooping();
   }
 
-  static void itsLooping(){
-    int myArr[] = {1,2,3,4,5,6,7};
+  static void itsLooping() {
+    int myArr[] = { 1, 2, 3, 4, 5, 6, 7 };
     for (int i = 0; i < myArr.length; i++) {
-      System.out.println(i+1);
+      System.out.println(i + 1);
     }
   }
 
 }
 
+class arrays {
+  public static void main(String[] args) {
+    // heyArray();
+    // char vowels[] = new char[5];
+    // vowels[1] = 'a';
+    // vowels[2] = 'e';
+    // vowels[3] = 'i';
+    // vowels[4] = 'o';
+    // vowels[5] = 'u';
+    // System.out.println(Arrays.toString(vowels));
+    heyarr();
+    sortingArr();
+  }
+
+  static void heyArray() {
+    // declaring the array
+    int array1[] = { 1, 2, 3, 4, 5, 6 };
+    System.out.println(array1);
+    System.out.println(Arrays.toString(array1));
+    for (int i : array1) {
+      System.out.println(i);
+    }
+
+    // another way of declaring an array
+
+    System.out.println(array1.length);
+
+    // to sort the array
+    // to make a copy
+    Arrays.copyOf(array1, 2);
+    System.out.println(array1);
+  }
+
+  static void heyarr() {
+    int myarr[] = { 1, 2, 3, 4, 5, 6, 7 };
+    System.out.println(Arrays.toString(myarr));
+
+    int copyarr[] = Arrays.copyOf(myarr, myarr.length);
+    System.out.println(Arrays.toString(copyarr));
+
+    System.out.println(Arrays.toString(copyarr));
+
+    copyarr[2] = 2;
+    System.out.println(Arrays.toString(myarr));
+    System.out.println(Arrays.toString(copyarr));
+
+  }
+
+  static void sortingArr() {
+    int hello[] = { 4, 3, 2, 1 };
+    System.out.println(Arrays.toString(hello));
+    Arrays.sort(hello);
+    System.out.println(hello);
+    System.out.println(Arrays.toString(hello));
+  }
+}
